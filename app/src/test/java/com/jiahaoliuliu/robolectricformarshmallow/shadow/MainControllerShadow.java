1dcfcbedc268c581fbde9e4cb05db14a6e71bf27
Jiahao Liu <jiahao.liu@dubizzle.com>
2016-07-18 11:40:09 +0400

    Correctly shadowed the constructor

--- app/src/test/java/com/jiahaoliuliu/robolectricformarshmallow/shadow/MainControllerShadow.java
+++ app/src/test/java/com/jiahaoliuliu/robolectricformarshmallow/shadow/MainControllerShadow.java
@@ -1,5 +1,6 @@
 package com.jiahaoliuliu.robolectricformarshmallow.shadow;
 
+import android.content.Context;
 import com.jiahaoliuliu.robolectricformarshmallow.controller.MainController;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
@@ -9,6 +10,10 @@ import org.robolectric.annotation.Implements;
  */
 @Implements(MainController.class)
 public class MainControllerShadow {
+
+    public void __constructor__ (Context context) {
+        // Not do anything
+    }
 
     @Implementation
     public String getTextToDisplay(boolean permissionGranted) {
